1d615b736d0ca8120633e48c2218940659a709a3
Keerthi Vasan <52066465+vile-PaL@users.noreply.github.com>
2025-06-05 00:35:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,6 +11,6 @@ module.exports = {
         clientId: "a568b55af1d940aca52ea8fe02f0d93b",
         clientSecret: "e8199f4024fe49c5b22ea9a3dd0c4789"
     },
-    botToken: "MTM3Njk2ODY1NDIwOTY4MzU1Nw.GPPAXR.cYKLEWktZmgfD9hJJ26-UXeMB4lAahia6FhIAA",
+    botToken: "",
     embedColor: "#0061ff"
 };
